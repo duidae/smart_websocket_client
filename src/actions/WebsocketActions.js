23c8f26ee9b85eae127eca208462721d0c1b55de
duidae <duidae@localhost.localdomain>
2018-08-21 13:49:12 +0800

    Refactor send message part, move some code from WebsocketSource to WebsocketAction, simplify WebsocketSource

--- src/actions/WebsocketActions.js
+++ src/actions/WebsocketActions.js
@@ -2,7 +2,15 @@ import alt from '../alt';
 import * as constants from '../sources/WebsocketSource';
 import WebsocketSource from '../sources/WebsocketSource';
 
+import {CARTA} from 'carta-protobuf';
+
+var self;
+
 class WebsocketActions {
+  constructor() {
+    self = this;
+  }
+
   initWebsocket() {
     WebsocketSource.on(constants.OPEN_EVENT, this.actions.websocketOpened);
     WebsocketSource.on(constants.CLOSE_EVENT, this.actions.websocketClosed);
@@ -25,7 +33,26 @@ class WebsocketActions {
   }
 
   sendData(contentStore) {
-    WebsocketSource.send(contentStore);
+    // handling payload data, encode with protobuf or not
+    let eventData = '';
+    if(contentStore.useProtobuf === true) { // encode protobuf here
+      eventData = self._encodeEvent(contentStore.eventName, contentStore.eventId, contentStore.payload);
+    }
+    else {
+      eventData = contentStore.eventName + ' ' + contentStore.eventId + ' ' + contentStore.payload;
+    }
+
+    // send data via WebsocketSource
+    WebsocketSource.send(eventData);
+
+    // debug message
+    if(__DEV__) {
+      console.log('Using protobuf:', contentStore.useProtobuf);
+      console.log('Eventname:', contentStore.eventName);
+      console.log('EventId:', contentStore.eventId);
+      console.log('Payload:', contentStore.payload);
+      console.log('[Client sent]: ', eventData);
+    }
   }
 
   dataSent(request) {
@@ -60,6 +87,48 @@ class WebsocketActions {
 
   requestDataChanged(data) {
     this.dispatch(data);
+  }
+
+  _encodeEvent(eventName, eventId, payload) {
+    let encodedMessage = '';
+
+    if (eventName === "REGISTER_VIEWER") {
+      encodedMessage = CARTA.RegisterViewer.encode(payload).finish();
+    }
+    else if (eventName === "FILE_LIST_REQUEST") {
+      encodedMessage = CARTA.FileListRequest.encode(payload).finish();
+    }
+    else if (eventName === "FILE_INFO_REQUEST") {
+      encodedMessage = CARTA.FileInfoRequest.encode(payload).finish();
+    }
+    else if (eventName === "OPEN_FILE") {
+      encodedMessage = CARTA.OpenFile.encode(payload).finish();
+    }
+    else if (eventName === "CLOSE_FILE") {
+      encodedMessage = CARTA.CloseFile.encode(payload).finish();
+    }
+    else if (eventName === "SET_IMAGE_VIEW") {
+      encodedMessage = CARTA.SetImageView.encode(payload).finish();
+    }
+    else {
+      console.log(`Unsupported event response ${eventName}`);
+    }
+
+    let eventData = '';
+    eventData = new Uint8Array(32 + 4 + encodedMessage.byteLength);
+    eventData.set(self._stringToUint8Array(eventName, 32));
+    eventData.set(new Uint8Array(new Uint32Array([eventId]).buffer), 32);
+    eventData.set(encodedMessage, 36);
+    return eventData;
+  }
+
+  _stringToUint8Array(str, padLength) {
+    const bytes = new Uint8Array(padLength);
+    for (let i = 0; i < Math.min(str.length, padLength); i++) {
+        const charCode = str.charCodeAt(i);
+        bytes[i] = (charCode <= 0xFF ? charCode : 0);
+    }
+    return bytes;
   }
 }
 
--- src/sources/WebsocketSource.js
+++ src/sources/WebsocketSource.js
@@ -1,8 +1,6 @@
 import assign from 'object-assign'
 import EventEmitter from 'events'
 
-import {CARTA} from 'carta-protobuf';
-
 export const OPEN_EVENT = 'open';
 export const CLOSE_EVENT = 'close';
 export const ERROR_EVENT = '_error'; //event cannot just be 'error', or the EventEmitter will raise an error.
@@ -29,23 +27,8 @@ class WebsocketSource {
     return this;
   }
 
-  send(contentStore) {
-    let eventData = '';
-    if(contentStore.useProtobuf) { // encode protobuf here
-      //eventData = contentStore.eventName + ' ' + contentStore.eventId + ' ' + '=== protobuf under construction ===';
-      eventData = this._encodeEvent(contentStore.eventName, contentStore.eventId, contentStore.payload);
-    }
-    else {
-      eventData = contentStore.eventName + ' ' + contentStore.eventId + ' ' + contentStore.payload;
-    }
+  send(eventData) {
     if (this.connection && this.connection.readyState === WebSocket.OPEN) {
-      if(__DEV__) {
-        console.log('Using protobuf:', contentStore.useProtobuf);
-        console.log('Eventname:', contentStore.eventName);
-        console.log('EventId:', contentStore.eventId);
-        console.log('Payload:', contentStore.payload);
-        console.log('[Client sent]: ', eventData);
-      }
       this.emit(SENT_EVENT, {
         address: this.connection.url,
         type: "Client sent",
@@ -87,48 +70,6 @@ class WebsocketSource {
       type: "Server respond",
       data: event.data
     });
-  }
-
-  _encodeEvent(eventName, eventId, payload) {
-    let encodedMessage = '';
-
-    if (eventName === "REGISTER_VIEWER") {
-      encodedMessage = CARTA.RegisterViewer.encode(payload).finish();
-    }
-    else if (eventName === "FILE_LIST_REQUEST") {
-      encodedMessage = CARTA.FileListRequest.encode(payload).finish();
-    }
-    else if (eventName === "FILE_INFO_REQUEST") {
-      encodedMessage = CARTA.FileInfoRequest.encode(payload).finish();
-    }
-    else if (eventName === "OPEN_FILE") {
-      encodedMessage = CARTA.OpenFile.encode(payload).finish();
-    }
-    else if (eventName === "CLOSE_FILE") {
-      encodedMessage = CARTA.CloseFile.encode(payload).finish();
-    }
-    else if (eventName === "SET_IMAGE_VIEW") {
-      encodedMessage = CARTA.SetImageView.encode(payload).finish();
-    }
-    else {
-      console.log(`Unsupported event response ${eventName}`);
-    }
-
-    let eventData = '';
-    eventData = new Uint8Array(32 + 4 + encodedMessage.byteLength);
-    eventData.set(this._stringToUint8Array(eventName, 32));
-    eventData.set(new Uint8Array(new Uint32Array([eventId]).buffer), 32);
-    eventData.set(encodedMessage, 36);
-    return eventData;
-  }
-
-  _stringToUint8Array(str, padLength) {
-    const bytes = new Uint8Array(padLength);
-    for (let i = 0; i < Math.min(str.length, padLength); i++) {
-        const charCode = str.charCodeAt(i);
-        bytes[i] = (charCode <= 0xFF ? charCode : 0);
-    }
-    return bytes;
   }
 }
 
